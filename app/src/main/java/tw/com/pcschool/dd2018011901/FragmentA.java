4f0a0163a5e0cf4af6f42a41595074c37eae7a22
Teacher <shaojung@gmail.com>
2018-01-19 10:18:14 +0800

    Change textview on another fragment.

--- app/src/main/java/tw/com/pcschool/dd2018011901/FragmentA.java
+++ app/src/main/java/tw/com/pcschool/dd2018011901/FragmentA.java
@@ -3,10 +3,13 @@ package tw.com.pcschool.dd2018011901;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -31,6 +34,19 @@ public class FragmentA extends Fragment {
 
     public FragmentA() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button btn = getActivity().findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tv = getActivity().findViewById(R.id.textView);
+                tv.setText("Hello World");
+            }
+        });
     }
 
     /**
